6a9021cf97d9d5944efcef82f9fcf47bd203423e
koheiji552000 <phpphp5520000@yahoo.co.jp>
2018-06-29 08:49:50 +0900

    サイドバーテンプレート化
    ・blog/index.phpのサイドバーを切り離しtemplates/sidebar.phpに変更
    ・設定ファイルにテンプレート用フォルダに対しての定数を追加

--- config.php
+++ config.php
@@ -4,4 +4,7 @@ ini_set("date.timezone", "Asia/Tokyo");
 mb_language("japanese");
 mb_internal_encoding("UTF-8");
 
+// コンテンツサーバーURL
 define('CONTENTS_SERVER_URL', 'http://c.blog-system-725.localhost');
+// テンプレートファイルパス
+define('TEMPLATE_PATH', 'C:/Dropbox/develop/blog-system-725/app/templates');

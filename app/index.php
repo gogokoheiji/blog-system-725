f934dedb38bfb0238a4f6b4d16e428411b68afcc
phpphp552 <phpphp552000@yahoo.co.jp>
2018-08-12 00:45:32 +0900

    ログインチェック機能修正など
    ・ログインチェック機能修正
    ・サインアップページの修正

--- app/index.php
+++ app/index.php
@@ -8,13 +8,18 @@ try {
 	session_start();
 	$request_path = $_REQUEST['path'];
 
-	// ログインチェック
+	// サインアップページの場合はログインチェック無し
 	if ($request_path == '/signup/') {
 		include(dirname(__FILE__).'/models/client/signup.php');
 	} else {
-		if (isset($url_list[$request_path])) {
-			// アクセスされたURLのプログラムに処理を移譲
-			include(dirname(__FILE__).$url_list[$request_path]);
+		// ログインチェック
+		if (TRUE) {
+			include(dirname(__FILE__).'/models/client/login.php');
+		} else {
+			if (isset($url_list[$request_path])) {
+				// アクセスされたURLのプログラムに処理を移譲
+				include(dirname(__FILE__).$url_list[$request_path]);
+			}
 		}
 	}
 } catch (Exception $e) {
--- app/models/client/signup.php
+++ app/models/client/signup.php
@@ -5,10 +5,10 @@
 <!--<![endif]-->
 <head>
 	<meta charset="utf-8" />
-	<title>サインアップ | BLOG SYSTEM</title>
+	<title>アカウント作成 | BLOG SYSTEM</title>
 	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
-	<meta content="" name="description" />
-	<meta content="" name="author" />
+	<meta content="Blog System Demonstration" name="description" />
+	<meta content="KOHEIJI BLOG" name="author" />
 
 	<!-- ================== BEGIN BASE CSS STYLE ================== -->
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
@@ -24,6 +24,17 @@
 	<!-- ================== BEGIN BASE JS ================== -->
 	<script src="<?php echo CONTENTS_SERVER_URL ?>/assets/plugins/pace/pace.min.js"></script>
 	<!-- ================== END BASE JS ================== -->
+
+	<link rel="icon" type="image/vnd.microsoft.icon" href="https://demo.flu-x.net/contents/img/favicon.ico">
+	<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="https://demo.flu-x.net/contents/img/favicon.ico">
+	<link rel="apple-touch-icon" sizes="180x180" href="https://demo.flu-x.net/contents/img/apple-touch-icon-180x180.png">
+
+	<style>
+	.border-top {border-top: 1px solid #dcdcdc!important;}
+	.border-bottom {border-bottom: 1px solid #dcdcdc!important;}
+	.border-left {border-left: 1px solid #dcdcdc!important;}
+	.border-right {border-right: 1px solid #dcdcdc!important;}
+	</style>
 </head>
 <body class="pace-top bg-white">
 	<!-- begin #page-loader -->
@@ -38,9 +49,9 @@
             <div class="news-feed">
                 <div class="news-image" style="background-image: url(<?php echo CONTENTS_SERVER_URL ?>/assets/img/login-bg/login-bg-9.jpg)"></div>
                 <div class="news-caption">
-                    <h4 class="caption-title"><b>Color</b> Admin App</h4>
+                    <h4 class="caption-title"><b>BLOG SYSTEM</b></h4>
                     <p>
-                        As a Color Admin app administrator, you use the Color Admin console to manage your organization’s account, such as add new users, manage security settings, and turn on the services you want your team to access.
+                        Blog System Demonstration
                     </p>
                 </div>
             </div>
@@ -49,58 +60,59 @@
             <div class="right-content">
                 <!-- begin register-header -->
                 <h1 class="register-header">
-                    Sign Up
-                    <small>Create your Color Admin Account. It’s free and always will be.</small>
+                    アカウント作成
+                    <small>Create your FLUX Account.</small>
                 </h1>
                 <!-- end register-header -->
                 <!-- begin register-content -->
                 <div class="register-content">
                     <form action="index.html" method="GET" class="margin-bottom-0">
-                        <label class="control-label">Name <span class="text-danger">*</span></label>
+                        <label class="control-label">アカウント名 <span class="text-danger">*</span></label>
+                        <div class="row m-b-15">
+                            <div class="col-md-12">
+                                <input type="text" class="form-control" name="client_name" value="" placeholder="" />
+                            </div>
+                        </div>
+                        <label class="control-label">メールアドレス <span class="text-danger">*</span></label>
+                        <div class="row m-b-15">
+                            <div class="col-md-12">
+                                <input type="text" class="form-control" name="mail_address" value="" placeholder="" />
+                            </div>
+                        </div>
+                        <label class="control-label">パスワード <span class="text-danger">*</span></label>
                         <div class="row row-space-10">
                             <div class="col-md-6 m-b-15">
-                                <input type="text" class="form-control" placeholder="First name" required />
+                                <input type="password" class="form-control"  name="password" value="" placeholder="8文字以上" />
                             </div>
-                            <div class="col-md-6 m-b-15">
-                                <input type="text" class="form-control" placeholder="Last name" required />
-                            </div>
-                        </div>
-                        <label class="control-label">Email <span class="text-danger">*</span></label>
-                        <div class="row m-b-15">
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" placeholder="Email address" required />
+							<div class="col-md-6 m-b-15">
+                                <input type="password" class="form-control"  name="password_confirm" value="" placeholder="再入力" />
                             </div>
                         </div>
-                        <label class="control-label">Re-enter Email <span class="text-danger">*</span></label>
+                        <label class="control-label">招待コード <span class="text-danger">*</span></label>
                         <div class="row m-b-15">
                             <div class="col-md-12">
-                                <input type="text" class="form-control" placeholder="Re-enter email address" required />
-                            </div>
-                        </div>
-                        <label class="control-label">Password <span class="text-danger">*</span></label>
-                        <div class="row m-b-15">
-                            <div class="col-md-12">
-                                <input type="password" class="form-control" placeholder="Password" required />
+                                <input type="text" class="form-control" name="secret_code" value="" placeholder="招待コードをお持ちの方のみがご登録頂けます。" />
                             </div>
                         </div>
                         <div class="checkbox checkbox-css m-b-30">
                         	<div class="checkbox checkbox-css m-b-30">
-								<input type="checkbox" id="agreement_checkbox" value="">
-								<label for="agreement_checkbox">
-									By clicking Sign Up, you agree to our <a href="javascript:;">Terms</a> and that you have read our <a href="javascript:;">Data Policy</a>, including our <a href="javascript:;">Cookie Use</a>.
+								<input type="checkbox" id="agreement" name="agreement" value="1">
+								<label for="agreement">
+									<a href="javascript:;">利用規約</a>及び<a href="javascript:;">プライバシーポリシー</a>に同意します。
 								</label>
 							</div>
                         </div>
                         <div class="register-buttons">
-                            <button type="submit" class="btn btn-primary btn-block btn-lg">Sign Up</button>
+                            <button type="submit" class="btn btn-primary btn-block btn-lg">アカウント作成</button>
                         </div>
                         <div class="m-t-20 m-b-40 p-b-40 text-inverse">
-                            Already a member? Click <a href="login_v3.html">here</a> to login.
+                            既にアカウントをお持ちの方は<a href="/login/">こちら</a>
                         </div>
                         <hr />
                         <p class="text-center">
-                            &copy; Color Admin All Right Reserved 2018
+                            &copy;2018 KOHEIJI BLOG All Rights Reserved.
                         </p>
+						<input type="hidden" name="FLUXDEMOTOKEN" value="" />
                     </form>
                 </div>
                 <!-- end register-content -->
@@ -108,117 +120,6 @@
             <!-- end right-content -->
         </div>
         <!-- end register -->
-
-        <!-- begin theme-panel -->
-        <div class="theme-panel theme-panel-lg">
-            <a href="javascript:;" data-click="theme-panel-expand" class="theme-collapse-btn"><i class="fa fa-cog"></i></a>
-            <div class="theme-panel-content">
-                <h5 class="m-t-0">Color Theme</h5>
-                <ul class="theme-list clearfix">
-                    <li><a href="javascript:;" class="bg-red" data-theme="red" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/red.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Red">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-pink" data-theme="pink" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/pink.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Pink">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-orange" data-theme="orange" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/orange.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Orange">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-yellow" data-theme="yellow" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/yellow.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Yellow">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-lime" data-theme="lime" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/lime.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Lime">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-green" data-theme="green" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/green.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Green">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-teal" data-theme="default" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/default.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Default">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-aqua" data-theme="aqua" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/aqua.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Aqua">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-blue" data-theme="blue" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/blue.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Blue">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-purple" data-theme="purple" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/purple.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Purple">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-indigo" data-theme="indigo" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/indigo.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Indigo">&nbsp;</a></li>
-                    <li><a href="javascript:;" class="bg-black" data-theme="black" data-theme-file="<?php echo CONTENTS_SERVER_URL ?>/assets/css/default/theme/black.css" data-click="theme-selector" data-toggle="tooltip" data-trigger="hover" data-container="body" data-title="Black">&nbsp;</a></li>
-                </ul>
-                <div class="divider"></div>
-                <div class="row m-t-10">
-                    <div class="col-md-6 control-label text-inverse f-w-600">Header Styling</div>
-                    <div class="col-md-6">
-                        <select name="header-styling" class="form-control form-control-sm">
-                            <option value="1">default</option>
-                            <option value="2">inverse</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row m-t-10">
-                    <div class="col-md-6 control-label text-inverse f-w-600">Header</div>
-                    <div class="col-md-6">
-                        <select name="header-fixed" class="form-control form-control-sm">
-                            <option value="1">fixed</option>
-                            <option value="2">default</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row m-t-10">
-                    <div class="col-md-6 control-label text-inverse f-w-600">Sidebar Styling</div>
-                    <div class="col-md-6">
-                        <select name="sidebar-styling" class="form-control form-control-sm">
-                            <option value="1">default</option>
-                            <option value="2">grid</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row m-t-10">
-                    <div class="col-md-6 control-label text-inverse f-w-600">Sidebar</div>
-                    <div class="col-md-6">
-                        <select name="sidebar-fixed" class="form-control form-control-sm">
-                            <option value="1">fixed</option>
-                            <option value="2">default</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row m-t-10">
-                    <div class="col-md-6 control-label text-inverse f-w-600">Sidebar Gradient</div>
-                    <div class="col-md-6">
-                        <select name="content-gradient" class="form-control form-control-sm">
-                            <option value="1">disabled</option>
-                            <option value="2">enabled</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row m-t-10">
-                    <div class="col-md-6 control-label text-inverse f-w-600">Content Styling</div>
-                    <div class="col-md-6">
-                        <select name="content-styling" class="form-control form-control-sm">
-                            <option value="1">default</option>
-                            <option value="2">black</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row m-t-10">
-                    <div class="col-md-6 control-label text-inverse f-w-600">Direction</div>
-                    <div class="col-md-6">
-                        <select name="direction" class="form-control form-control-sm">
-                            <option value="1">LTR</option>
-                            <option value="2">RTL</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="divider"></div>
-                <h5>THEME VERSION</h5>
-                <div class="theme-version">
-                	<a href="<?php echo CONTENTS_SERVER_URL ?>/template_html/index_v2.html" class="active">
-                		<span style="background-image: url(<?php echo CONTENTS_SERVER_URL ?>/assets/img/theme/default.jpg);"></span>
-                	</a>
-                	<a href="<?php echo CONTENTS_SERVER_URL ?>/template_transparent/index_v2.html">
-                		<span style="background-image: url(<?php echo CONTENTS_SERVER_URL ?>/assets/img/theme/transparent.jpg);"></span>
-                	</a>
-                </div>
-                <div class="theme-version">
-                	<a href="<?php echo CONTENTS_SERVER_URL ?>/template_apple/index_v2.html">
-                		<span style="background-image: url(<?php echo CONTENTS_SERVER_URL ?>/assets/img/theme/apple.jpg);"></span>
-                	</a>
-                	<a href="<?php echo CONTENTS_SERVER_URL ?>/template_material/index_v2.html">
-                		<span style="background-image: url(<?php echo CONTENTS_SERVER_URL ?>/assets/img/theme/material.jpg);"></span>
-                	</a>
-                </div>
-                <div class="divider"></div>
-                <div class="row m-t-10">
-                    <div class="col-md-12">
-                        <a href="javascript:;" class="btn btn-inverse btn-block btn-rounded" data-click="reset-local-storage"><b>Reset Local Storage</b></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end theme-panel -->
 	</div>
 	<!-- end page container -->
 
@@ -240,6 +141,62 @@
 	<script>
 		$(document).ready(function() {
 			App.init();
+		});
+
+		function form_submit(mode, code, message) {
+			if (confirm(message)) {
+				document.mainform.mode.value = mode;
+				document.mainform.code.value = code;
+				document.mainform.submit();
+			}
+		}
+
+		$('[data-click="delete-confirm"]').click(function(e) {
+			e.preventDefault();
+			swal({
+				title: '削除してもよろしいですか？',
+				text: '削除したデータは元には戻せません。',
+				icon: 'error',
+				buttons: {
+					cancel: {
+						text: 'キャンセル',
+						value: null,
+						visible: true,
+						className: 'btn btn-default',
+						closeModal: true,
+					},
+					confirm: {
+						text: '削除',
+						value: true,
+						visible: true,
+						className: 'btn btn-danger',
+						closeModal: true
+					}
+				}
+			})
+			.then((willDelete) => {
+				if (willDelete) {
+					var delete_code = $(this).data('id');
+
+					var formData = new FormData(document.getElementById("mainform"));
+					formData.append('mode', 'delete');
+					formData.append('code', delete_code);
+
+					$.ajax({
+						type: "POST",
+						url: "/signup",
+						data: formData,
+						processData: false,
+						contentType: false,
+						success: function(xml) {
+							var check_status = $(xml).find("status").text();
+							if (check_status == 1) {
+								$("#item_"+delete_code).remove();
+							}
+						}
+					});
+				}
+			});
 		});
 	</script>
 </body>
